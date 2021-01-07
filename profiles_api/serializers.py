99418cbee2c608b466b9c15da0944715c9f3c541
Zdravko <zdravkobonev@abv.bg>
2021-01-07 17:22:55 +0200

    Added user profile feed API

--- profiles_api/serializers.py
+++ profiles_api/serializers.py
@@ -39,10 +39,10 @@ class UserProfileSerializer(serializers.ModelSerializer):
 
         return super().update(instance, validated_data)
 
-# class ProfileFeedItemSerializer(serializers.ModelSerializer):
-#     """"Serializes profile feed items""""
-#
-#     class Meta:
-#         model = models.ProfileFeedItem
-#         fields = ('id', 'user_profile', 'status_text', 'created_on')
-#         extra_kwargs = {'user_profile': {'read_only': True}}
+class ProfileFeedItemSerializer(serializers.ModelSerializer):
+    """"Serializes profile feed items"""
+
+    class Meta:
+        model = models.ProfileFeedItem
+        fields = ('id', 'user_profile', 'status_text', 'created_on')
+        extra_kwargs = {'user_profile': {'read_only': True}}
